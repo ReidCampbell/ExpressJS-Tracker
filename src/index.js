488827be805769a2b51eae43e724321672aafc56
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-24 17:55:54 +0100

    added user authentication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+require("./models/User");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hi there");
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+    "mongodb+srv://ReidCampbell:lV6VwRZr0GDw7BGI@cluster0-2zhlg.mongodb.net/test?retryWrites=true&w=1;";
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on("connected", () => {
+    console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", err => {
+    console.error("Error connection to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
